676c63adeb5536323614f492c63e12be75d18b05
Pape Saliou Ka <papesaliouka@gmail.com>
2022-01-24 13:30:25 +0000

    finnish bulding search component and search functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,15 @@ import {FormsModule, } from '@angular/forms';
 import {HeroDetailComponent} from './hero-detail/hero-detail.component';
 import {MessagesComponent} from './messages/messages.component';
 import {AppRoutingModule} from './app-routing.module';
-import {DashboardComponent} from './dashboard/dashboard.component'; @NgModule({
+import {DashboardComponent} from './dashboard/dashboard.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component'; @NgModule({
     declarations: [
         AppComponent,
         HeroesComponent,
         HeroDetailComponent,
         MessagesComponent,
-        DashboardComponent
+        DashboardComponent,
+        HeroSearchComponent
     ],
     imports: [
         BrowserModule,
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -79,4 +79,16 @@ export class HeroService {
             catchError(this.handleError<Hero>('deleteHero'))
         )
     }
+
+    searchHero(term: string): Observable<Hero[]> {
+        if (!term.trim) return of([]);
+
+        return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+            tap(x => x.length ?
+                this.log(`found heroes match ${term}`) :
+                this.log(`no heroes matching ${term}`)
+            ),
+            catchError(this.handleError<Hero[]>('searchHeroes', []))
+        )
+    }
 }
